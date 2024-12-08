e99d88e0ca2701ac49160dcc4328a1693430760f
adavize123 <adavizeabdullahi@gmail.com>
2024-12-08 11:37:54 +0100

    my first commit

---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:608d5bd837780e627893e95ff5e9ccc00cd4817eb2d13a8544904f87931aea1e
+size 1326
---
+++ tb_detection_model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9d19e4b4a58ff7d9e78d5bf336576c71df2e7c6b33bf36118c3c90b9713136fc
+size 1865
